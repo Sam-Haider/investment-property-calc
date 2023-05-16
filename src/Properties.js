54b0503a8b2107e71c56fa1e94419be4c0cad77d
Sam-Haider <sammhaider0@gmail.com>
2023-05-16 00:37:01 -0400

    Refactor to functional component

--- src/Properties.js
+++ src/Properties.js
@@ -1,114 +1,94 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import PropertyForm from "./PropertyForm";
-import React from "react";
-import "./Properties.css";
 import getRandomImage from "./utils/getRandomImage";
-const axios = require("axios").default;
+import "./Properties.css";
 
-class Properties extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      allProperties: [],
-    };
-    this.getProperties = this.getProperties.bind(this);
-  }
+const Properties = () => {
+  const [allProperties, setAllProperties] = useState([]);
 
-  getProperties() {
-    let api = "http://localhost:8081";
-    if (window.location.hostname !== "localhost") {
-      api = "https://investmentpropcalcapi.herokuapp.com";
+  useEffect(() => {
+    getProperties();
+  }, []);
+
+  const getProperties = async () => {
+    const api =
+      window.location.hostname === "localhost"
+        ? "http://localhost:8081"
+        : "https://investmentpropcalcapi.herokuapp.com";
+
+    try {
+      const response = await axios.get(`${api}/property`);
+      setAllProperties(response.data);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
+
+  const renderProperties = () => {
+    if (allProperties.length === 0) {
+      return <p>No properties found.</p>;
     }
 
-    const getPropertiesFromApi = async () => {
-      try {
-        const result = await axios.get(`${api}/property`);
-        await this.setState({
-          allProperties: result.data,
-        });
-      } catch (e) {
-        console.log("Error", e);
-      }
-    };
-    getPropertiesFromApi();
-  }
+    return allProperties.map((property) => (
+      <li key={property._id} className="property-container">
+        <div className="property-image">
+          <img src={getRandomImage()} alt="a home" height="200" />
+        </div>
+        <div className="property-details">
+          <h3>{property.propertyAddress}</h3>
+          <div>
+            <b>Purchase Price: </b>${property.purchasePrice}
+          </div>
+          <div>
+            <b>Down Payment: </b>${property.downPayment}
+          </div>
+          <div>
+            <b>Interest Rate: </b>
+            {property.interestRate}%
+          </div>
+          <div>
+            <b>Loan Term: </b>
+            {property.loanTerm} Years
+          </div>
+          <div>
+            <b>Monthly Rental Income: </b>${property.rentalIncome}
+          </div>
+          <div>
+            <b>Monthly Expenses: </b>${property.expenses}
+          </div>
+          <hr />
+          <h2>Analysis:</h2>
+          <div>
+            <b>Loan Amount: </b>${property.purchasePrice - property.downPayment}
+          </div>
+          <div>
+            <b>Monthly Mortgage Payment: </b>${property.mortgagePayment}
+          </div>
+          <div>
+            <b>Total Monthly Expenses (incl. Mortgage): </b>$
+            {property.expenses + property.mortgagePayment}
+          </div>
+          <div>
+            <b>Total Monthly Cash Flow: </b>${property.cashFlow}
+          </div>
+          <div>
+            <b>Cash on Cash Return: </b>
+            {property.cocReturn}%
+          </div>
+        </div>
+      </li>
+    ));
+  };
 
-  componentDidMount() {
-    this.getProperties();
-    console.log("while mounting");
-    getRandomImage();
-  }
-
-  render() {
-    const { allProperties } = this.state;
-    return (
-      <>
-        <h2 className="section-title">Submit a New Property:</h2>
-        <PropertyForm getProperties={this.getProperties} />
-        <h2 className="section-title">Saved Properties:</h2>
-        <ul>
-          {allProperties.map((property) => (
-            <li key={property._id} className="property container">
-              <div className="row check">
-                <div className="col-lg-3">
-                  <img src={getRandomImage()} alt="a home" height="200" />
-                </div>
-
-                <div className="data-provided col-lg-9 container">
-                  <div className="row post-details">
-                    <div className="col-lg-6">
-                      <h3>{property.propertyAddress}</h3>
-                      <div>
-                        <b>Purchase Price: </b>${property.purchasePrice}
-                      </div>
-                      <div>
-                        <b>Down Payment: </b>${property.downPayment}
-                      </div>
-                      <div>
-                        <b>Interest Rate: </b> {property.interestRate}%
-                      </div>
-                      <div>
-                        <b>Loan Term: </b>${property.loanTerm} Years
-                      </div>
-                      <div>
-                        <b>Monthly Rental Income: </b>${property.rentalIncome}
-                      </div>
-                      <div>
-                        <b>Monthly Expenses: </b>${property.expenses}
-                      </div>
-                    </div>
-
-                    <div className="col-lg-6">
-                      <hr></hr>
-                      <h2>Analysis:</h2>
-                      <div>
-                        <b>Loan Amount </b>$
-                        {property.purchasePrice - property.downPayment}
-                      </div>
-                      <div>
-                        <b>Monthly Mortgage Payment: </b>$
-                        {property.mortgagePayment}
-                      </div>
-                      <div>
-                        <b>Total Monthly Expenses (incl Mortgage): </b>$
-                        {property.expenses + property.mortgagePayment}
-                      </div>
-                      <div>
-                        <b>Total Monthly Cash Flow </b>${property.cashFlow}
-                      </div>
-                      <div>
-                        <b>Cash on Cash Return </b>
-                        {property.cocReturn}%
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </>
-    );
-  }
-}
+  return (
+    <div className="properties-container">
+      <h2 className="section-title">Submit a New Property:</h2>
+      <PropertyForm getProperties={getProperties} />
+      <h2 className="section-title">Saved Properties:</h2>
+      <ul className="properties-list">{renderProperties()}</ul>
+    </div>
+  );
+};
 
 export default Properties;
